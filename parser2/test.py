b51abefc67e6bd787f34ea8cb6dceb0c7f388baf
Max_A <Max900000@yandex.ru>
2021-09-23 01:50:37 +0500

    added readme

--- parser2/test.py
+++
@@ -1,8 +0,0 @@
-import bs4
-from bs4.dammit import encoding_res
-import requests
-
-
-url = "https://www.vamsvet.ru/similar/artelamp_lyustra-a9310lm-8wg/?catalog_ajax_call=Y&PAGEN_1=2&mode=undefined"
-
-
